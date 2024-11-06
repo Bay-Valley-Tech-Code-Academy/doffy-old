a741c5dbab6681daf794107a09ad2b2d7f90281d
Kimberly Encinas <kimbencinas@gmail.com>
2024-11-06 12:42:38 -0800

    Add scraper for governmentjobs

---
+++ crawler/indeed_scraper.py
@@ -0,0 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_indeed():
+    url = 'https://www.indeed.com/jobs?q=tech+jobs&l='
---
+++ crawler/scraper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_monster():
+    url = 'https://www.monster.com/jobs/search?q=tech+jobs&where=california&page=1&so=m.h.s'  # Replace with the actual URL
+    
+    scrape_monster()
+
+def scrape_indeed():
+    url = "https://www.indeed.com/m/jobs?q=tech%20jobs&l="
+
+    scrape_indeed()
+
+def scrape_zip_recruiter():
+    url = "https://www.ziprecruiter.com/jobs-search?search=tech+jobs&location="
+
+    scrape_zip_recruiter()
+
+def scrape_government_jobs():
+    url = "https://www.governmentjobs.com/jobs?keyword=tech+jobs&location="
+    
+    scrape_government_jobs()
