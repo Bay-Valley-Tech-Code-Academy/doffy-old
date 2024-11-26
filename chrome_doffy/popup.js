b47789b1150b1201b023c8ea38f3b883b73025c1
Javier <javier.miranda4420@gmail.com>
2024-11-26 11:17:42 -0800

    got the chrome extension to scrape indeed

--- chrome_doffy/popup.js
+++ chrome_doffy/popup.js
@@ -2,23 +2,22 @@ document.getElementById("scrape-btn").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { command: "scrape" }, (response) => {
       if (response?.jobs?.length) {
-        console.log(response.jobs);
+        console.log("Scraped Jobs:", response.jobs);
         const blob = new Blob([JSON.stringify(response.jobs, null, 2)], {
           type: "application/json",
         });
         const url = URL.createObjectURL(blob);
 
-        // Trigger a download of the data
+        // Trigger file download
         chrome.downloads.download({
           url: url,
-          filename: "jobs.json",
+          filename: "indeed_jobs.json",
         });
 
         document.getElementById("status").innerText =
           "Jobs scraped and downloaded!";
       } else {
-        document.getElementById("status").innerText =
-          "No jobs found or failed to scrape!";
+        document.getElementById("status").innerText = "No jobs found!";
       }
     });
   });
